4bc074104afff1ae77f70dc3617626bac94bc6ae
Anton Barinov <55404783+Anton-Barinov@users.noreply.github.com>
2026-06-25 11:33:27 +0300

    fix: cron_push.php JsonLogger constructor needs array not string

--- api/scripts/cron_push.php
+++ api/scripts/cron_push.php
@@ -42,7 +42,7 @@ $driver = (string)($dbConfig['driver'] ?? 'sqlite');
 
 $subscriptions = new Api\Model\Notification\PushSubscriptionRepository($pdo);
 $queue = new Api\Model\Notification\PushDispatchQueueRepository($pdo);
-$logger = new Api\System\Library\Logger\JsonLogger($basePath . '/logs');
+$logger = new Api\System\Library\Logger\JsonLogger(['push' => $basePath . '/logs/cron_push.log']);
 
 $push = new Api\System\Library\Service\NotificationPushService($subscriptions, $queue, $logger, $config);
 
