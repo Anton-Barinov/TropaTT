8cc29790394c948870ad21a9c48fc3872c0a261a
Anton Barinov <55404783+Anton-Barinov@users.noreply.github.com>
2026-09-14 19:53:42 +0300

    chore(modules): ship the CRM without bundled modules and move module tests to fixtures

--- upload/modules/crm.slack-integration/api/migration/001_create_tables_rollback.sql
+++
@@ -1,4 +0,0 @@
--- Slack Integration Module: Rollback
-DROP TABLE IF EXISTS module_slack_deliveries;
-DROP TABLE IF EXISTS module_slack_rules;
-DROP TABLE IF EXISTS module_slack_connections;
--- upload/modules/crm.slack-integration/web/config/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-use Module\Crm\SlackIntegration\Controller\SlackPageController;
-
-return [
-    'module-slack-integration' => [SlackPageController::class, 'index'],
-];
